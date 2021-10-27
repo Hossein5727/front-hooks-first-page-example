02f520adabbe4e57bab12c1c80ca9484a751611c
HosseinGhiasi <hosseinghiasi5727@gmail.com>
2021-10-27 10:06:32 +0330

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import App3 from "./App3";
 import App4 from "./App4";
 import App5 from "./App5";
@@ -8,6 +9,6 @@ import './index.css'
 
 
 ReactDOM.render(
-    <App3 />,
+    <App />,
     document.getElementById('root')
 )
